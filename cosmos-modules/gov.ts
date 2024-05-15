73a079064f540ef00745d1ce4d66b7a185f500f3
arlai <arlai@moonkitt.com>
2024-05-15 08:37:09 +0200

    Completing logic

--- cosmos-modules/gov.ts
+++ cosmos-modules/gov.ts
@@ -1,4 +1,5 @@
 import type { QueryVotesRequest } from "moonkittjs/dist/codegen/cosmos/gov/v1/query"
+import { VoteOption } from "moonkittjs/dist/codegen/cosmos/gov/v1/gov"
 
 import { getRpcClient, getAllWithPagination } from "../utils/client"
 
@@ -12,4 +13,17 @@ export const getAllVotes = async (proposalId: number) => {
   const allVotesFn = await getAllWithPagination(client.cosmos.gov.v1.votes, "votes")
 
   return await allVotesFn(allVotesParams)
+}
+
+export const displayVoteOption = (option: VoteOption): string => {
+  if (option == VoteOption.VOTE_OPTION_ABSTAIN) {
+    return "ABSTAIN"
+  } else if (option == VoteOption.VOTE_OPTION_YES) {
+    return "YES"
+  } else if (option == VoteOption.VOTE_OPTION_NO) {
+    return "NO"
+  } else if (option == VoteOption.VOTE_OPTION_NO_WITH_VETO) {
+    return "NWV"
+  }
+  return "UNDEFINED"
 }
--- main.ts
+++ main.ts
@@ -1,10 +1,45 @@
 import { getAllDelegations } from "./cosmos-modules/staking"
-import { getAllVotes } from "./cosmos-modules/gov"
+import { getAllVotes, displayVoteOption } from "./cosmos-modules/gov"
 
 const runAll = async (): Promise<void> => {
-  const votes = await getAllVotes(922)
+  const ids = [920, 921, 922]
 
-  console.log(votes)
+  const result = await Promise.all(ids.map((id) => displayVotesForProp(id)))
+
+  console.error(result)
+}
+
+const displayVotesForProp = async (id: number, limit?: number): Promise<string> => {
+  const votes = await getAllVotes(id)
+
+  let counter = 1
+  for (const vote of votes) {
+    const delegations = await getAllDelegations(vote.voter)
+
+    let total = 0
+    for (const delegation of delegations) {
+      total += +delegation.balance.amount
+    }
+
+    for (const option of vote.options) {
+      console.log(
+        id +
+          "|" +
+          vote.voter +
+          "|" +
+          (total * +option.weight) / 1000000 +
+          "|" +
+          displayVoteOption(option.option)
+      )
+    }
+
+    counter++
+    if (limit && counter > limit) {
+      break
+    }
+  }
+
+  return "Prop " + id + ": " + votes.length + " votes"
 }
 
 runAll()
