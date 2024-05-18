61145cb7e68e82f430dd8938ad0e9ff563265b60
arlai <arlai@moonkitt.com>
2024-05-18 06:55:00 +0200

    Adding a "Fast" version (not working) Adding the ability to split data by validator as well

--- cosmos-modules/staking.ts
+++ cosmos-modules/staking.ts
@@ -1,7 +1,11 @@
-import type { QueryDelegatorDelegationsRequest } from "moonkittjs/dist/codegen/cosmos/staking/v1beta1/query"
+import type {
+  QueryDelegatorDelegationsRequest,
+  QueryValidatorDelegationsRequest,
+  QueryValidatorsRequest,
+} from "moonkittjs/dist/codegen/cosmos/staking/v1beta1/query"
 import { getRpcClient, getAllWithPagination } from "../utils/client"
 
-export const getAllDelegations = async (address: string) => {
+export const getVoterDelegations = async (address: string) => {
   const client = await getRpcClient()
 
   const allDelegationsParams: QueryDelegatorDelegationsRequest = {
@@ -13,4 +17,42 @@ export const getAllDelegations = async (address: string) => {
   )
 
   return await allDelegationsFn(allDelegationsParams)
+}
+
+export const getAllValDelegations = async (valAddress: string) => {
+  const client = await getRpcClient()
+
+  const allValidatorDelegationsParams: QueryValidatorDelegationsRequest = {
+    validatorAddr: valAddress,
+  }
+  const allValidatorDelegationsFn = await getAllWithPagination(
+    client.cosmos.staking.v1beta1.validatorDelegations,
+    "delegationResponses"
+  )
+
+  return await allValidatorDelegationsFn(allValidatorDelegationsParams)
+}
+
+export const getAllValidators = async () => {
+  const client = await getRpcClient()
+
+  const allValidatorsBondedParams: QueryValidatorsRequest = {
+    status: "BOND_STATUS_BONDED",
+  }
+  const allValidatorsUnbondedParams: QueryValidatorsRequest = {
+    status: "BOND_STATUS_UNBONDED",
+  }
+  const allValidatorsUnbondingParams: QueryValidatorsRequest = {
+    status: "BOND_STATUS_UNBONDING",
+  }
+
+  const allValidatorsFn = await getAllWithPagination(client.cosmos.staking.v1beta1.validators, "validators")
+
+  const result = await Promise.all(
+    [allValidatorsBondedParams, allValidatorsUnbondedParams, allValidatorsUnbondingParams].map((type) =>
+      allValidatorsFn(type)
+    )
+  )
+
+  return result.flat()
 }
--- main.ts
+++ main.ts
@@ -1,36 +1,102 @@
-import { getAllDelegations } from "./cosmos-modules/staking"
+import { getVoterDelegations, getAllValidators, getAllValDelegations } from "./cosmos-modules/staking"
 import { getAllVotes, displayVoteOption } from "./cosmos-modules/gov"
 
-const runAll = async (): Promise<void> => {
+import { BondStatus, type Validator } from "moonkittjs/dist/codegen/cosmos/staking/v1beta1/staking"
+
+interface ValidatorByValoper {
+  [key: string]: Validator
+}
+
+interface DelegationsByDelegator {
+  [key: string]: {
+    valoper: string
+    moniker: string
+    amount: string
+  }[]
+}
+
+// We use this as CSV separator as we need a character not used in monikers
+const CSV_SEPARATOR = "`"
+
+// For some reason, "fast" crashes my RPC server.
+// It should be able to work, so just leaving it here (though untested yet).
+const runAll = async (type: "fast" | "slow"): Promise<void> => {
   const ids = [920, 921, 922]
 
-  const result = await Promise.all(ids.map((id) => displayVotesForProp(id)))
+  let result: string[] = [""]
+
+  if (type === "fast") {
+    const validators = await getAllValidators()
+    const delegationsByDelegator = await getAllDelegationsByDelegator(validators)
+    result = await Promise.all(ids.map((id) => displayVotesForPropFast(id, delegationsByDelegator)))
+  } else if (type === "slow") {
+    const validatorsByValoper = await getAllValidatorsByValoper()
+    result = await Promise.all(ids.map((id) => displayVotesForPropSlow(id, undefined, validatorsByValoper)))
+  }
 
   console.error(result)
 }
 
-const displayVotesForProp = async (id: number, limit?: number): Promise<string> => {
+// If you want to show the detail of each vote "per delegator / validator pair"
+// (i.e. not aggregate all delegations to one row)
+// Then please add the validatorsByValoper
+const displayVotesForPropSlow = async (
+  id: number,
+  limit?: number,
+  validatorsByValoper?: ValidatorByValoper
+): Promise<string> => {
   const votes = await getAllVotes(id)
 
   let counter = 1
   for (const vote of votes) {
-    const delegations = await getAllDelegations(vote.voter)
+    const delegations = await getVoterDelegations(vote.voter)
 
-    let total = 0
-    for (const delegation of delegations) {
-      total += +delegation.balance.amount
-    }
+    // We want to detail each vote "per delegator / validator pair"
+    if (validatorsByValoper) {
+      for (const delegation of delegations) {
+        if (!(delegation.delegation.validatorAddress in validatorsByValoper)) {
+          console.error("Error: Cannot find validator: " + delegation.delegation.validatorAddress)
+          continue
+        }
 
-    for (const option of vote.options) {
-      console.log(
-        id +
-          "|" +
-          vote.voter +
-          "|" +
-          (total * +option.weight) / 1000000 +
-          "|" +
-          displayVoteOption(option.option)
-      )
+        for (const option of vote.options) {
+          console.log(
+            id +
+              CSV_SEPARATOR +
+              vote.voter +
+              CSV_SEPARATOR +
+              delegation.delegation.validatorAddress +
+              CSV_SEPARATOR +
+              validatorsByValoper[delegation.delegation.validatorAddress].description.moniker +
+              CSV_SEPARATOR +
+              (validatorsByValoper[delegation.delegation.validatorAddress].status ==
+              BondStatus.BOND_STATUS_BONDED
+                ? "Active"
+                : "Inactive") +
+              CSV_SEPARATOR +
+              (+delegation.balance.amount * +option.weight) / 1000000 +
+              CSV_SEPARATOR +
+              displayVoteOption(option.option)
+          )
+        }
+      }
+    } else {
+      let total = 0
+      for (const delegation of delegations) {
+        total += +delegation.balance.amount
+      }
+
+      for (const option of vote.options) {
+        console.log(
+          id +
+            CSV_SEPARATOR +
+            vote.voter +
+            CSV_SEPARATOR +
+            (total * +option.weight) / 1000000 +
+            CSV_SEPARATOR +
+            displayVoteOption(option.option)
+        )
+      }
     }
 
     counter++
@@ -42,4 +108,79 @@ const displayVotesForProp = async (id: number, limit?: number): Promise<string> 
   return "Prop " + id + ": " + votes.length + " votes"
 }
 
-runAll()
+const getAllDelegationsByDelegator = async (validators: Validator[]): Promise<DelegationsByDelegator> => {
+  const delegationsByDelegator: DelegationsByDelegator = {}
+
+  for (const validator of validators) {
+    const validatorDelegations = await getAllValDelegations(validator.operatorAddress)
+    for (const delegation of validatorDelegations) {
+      delegationsByDelegator[delegation.delegation.delegatorAddress].push({
+        valoper: validator.operatorAddress,
+        moniker: validator.description.moniker,
+        amount: delegation.balance.amount,
+      })
+    }
+  }
+
+  return delegationsByDelegator
+}
+
+const getAllValidatorsByValoper = async (): Promise<ValidatorByValoper> => {
+  const validatorsByValoper: ValidatorByValoper = {}
+  const validators = await getAllValidators()
+
+  for (const validator of validators) {
+    validatorsByValoper[validator.operatorAddress] = validator
+  }
+
+  return validatorsByValoper
+}
+
+const displayVotesForPropFast = async (
+  id: number,
+  delegationsByDelegator: DelegationsByDelegator,
+  limit?: number,
+  valAddress?: string
+): Promise<string> => {
+  const votes = await getAllVotes(id)
+
+  let counter = 1
+  for (const vote of votes) {
+    if (!(vote.voter in delegationsByDelegator)) {
+      console.error("Error: cannot find delegation of voter: " + vote.voter)
+      continue
+    }
+
+    const delegations = delegationsByDelegator[vote.voter]
+
+    let total = 0
+    for (const delegation of delegations) {
+      if (!valAddress || delegation.valoper == valAddress) {
+        total += +delegation.amount
+      }
+    }
+
+    for (const option of vote.options) {
+      if (total > 0) {
+        console.log(
+          id +
+            CSV_SEPARATOR +
+            vote.voter +
+            CSV_SEPARATOR +
+            (total * +option.weight) / 1000000 +
+            CSV_SEPARATOR +
+            displayVoteOption(option.option)
+        )
+      }
+    }
+
+    counter++
+    if (limit && counter > limit) {
+      break
+    }
+  }
+
+  return "Prop " + id + ": " + votes.length + " votes"
+}
+
+runAll("slow")
